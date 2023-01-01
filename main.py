af84d84c039cd1ff42e668d51db85a2f668953ca
Xtaiewzer <georgerodenko@yandex.ru>
2023-01-01 12:25:28 +0300

    0.0.1

--- main.py
+++ main.py
@@ -6,6 +6,30 @@ win_fps = 30
 pygame.init()
 screen = pygame.display.set_mode((win_width, win_height))
 clock = pygame.time.Clock()
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+start_pos = (0, 0)
+start_pos_flag = False
+end_pos = (0, 0)
+end_pos_flag = False
+
+
+def starting():
+    global start_pos, start_pos_flag
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        start_pos = event.pos
+        start_pos_flag = True
+        pygame.draw.rect(screen, BLUE, (start_pos[0], start_pos[1], 10, 10))
+
+
+def ending():
+    global end_pos, end_pos_flag
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        end_pos = event.pos
+        end_pos_flag = True
+        pygame.draw.rect(screen, RED, (end_pos[0], end_pos[1], 10, 10))
+
 
 running = True
 while running:
@@ -13,6 +37,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if not start_pos_flag:
+            starting()
+        elif not end_pos_flag:
+            ending()
+
     pygame.display.flip()
 
 pygame.quit()
