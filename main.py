e09ce408c74996d721b002b8d52c820f7c9324e3
ilia_leshchenko <xzrokokozx@gmail.com>
2023-01-03 12:02:37 +0300

    Drawing lines

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pygame
 
 win_width = 1280
 win_height = 800
-win_fps = 360
+win_fps = 60
 pygame.init()
 screen = pygame.display.set_mode((win_width, win_height))
 clock = pygame.time.Clock()
-BLUE = (0, 0, 255)
+BLUE = (100, 150, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+GRAY = (70, 70, 70)
+SIDE = 10
+HSIDE = SIDE // 2
+QSIDE = SIDE // 4
 char_image = pygame.image.load('character.png')
 
 start_pos = (0, 0)
@@ -16,6 +20,8 @@ start_pos_flag = False
 end_pos = (0, 0)
 end_pos_flag = False
 moving_flag = False
+pods = []
+lines = True
 
 
 def starting():
@@ -23,7 +29,7 @@ def starting():
     if event.type == pygame.MOUSEBUTTONDOWN:
         start_pos = event.pos
         start_pos_flag = True
-        pygame.draw.rect(screen, BLUE, (start_pos[0], start_pos[1], 10, 10))
+        pygame.draw.rect(screen, BLUE, (start_pos[0] - HSIDE, start_pos[1] - HSIDE, SIDE, SIDE))
 
 
 def ending():
@@ -31,14 +37,31 @@ def ending():
     if event.type == pygame.MOUSEBUTTONDOWN:
         end_pos = event.pos
         end_pos_flag = True
-        pygame.draw.rect(screen, RED, (end_pos[0], end_pos[1], 10, 10))
+        pygame.draw.rect(screen, RED, (end_pos[0] - HSIDE, end_pos[1] - HSIDE, SIDE, SIDE))
 
 
-def drawing():
+def drawing(e):
+    global lines
     pressed = pygame.mouse.get_pressed()
     pos = pygame.mouse.get_pos()
-    if pressed[0]:
-        pygame.draw.rect(screen, GREEN, (pos[0], pos[1], 10, 10))
+    if e.type == pygame.KEYDOWN and (e.mod & pygame.KMOD_CTRL):
+        lines = not lines
+    if lines:
+        drawing_lines(e, pos)
+    else:
+        if pressed[0]:
+            pygame.draw.circle(screen, GREEN, pos, HSIDE)
+
+
+def drawing_lines(e, p):
+    if e.type == pygame.MOUSEBUTTONDOWN:
+        button = e.button
+        if button == 1:
+            pods.append(p)
+            pygame.draw.circle(screen, GREEN, p, QSIDE)
+        elif button == 3 and len(pods) >= 2:
+            pygame.draw.lines(screen, GREEN, False, pods, HSIDE)
+            pods.clear()
 
 
 class Character(pygame.sprite.Sprite):
@@ -50,6 +73,8 @@ class Character(pygame.sprite.Sprite):
 
 
 pygame.display.set_caption('The Best Project Ever')
+screen.fill(GRAY)
+pygame.display.update()
 running = True
 while running:
     clock.tick(win_fps)
@@ -61,7 +86,7 @@ while running:
         elif not end_pos_flag:
             ending()
         elif start_pos_flag and end_pos_flag and not moving_flag:
-            drawing()
+            drawing(event)
             all_sprites = pygame.sprite.Group()
             char = Character()
             all_sprites.add(char)
